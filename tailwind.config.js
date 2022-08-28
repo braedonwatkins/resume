a75b89504e1cfa892fe01169b445ee28744c266c
BraedonWatkins <watkins.braedon@gmail.com>
2022-08-28 09:02:12 -0400

    if it's all in the same directory there can't be pathing issues >:D

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./docs/**/*.{html,js,jsx,ts,tsx}"],
+  content: ["*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
