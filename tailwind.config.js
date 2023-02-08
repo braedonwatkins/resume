5905aa8f8396f8c3dbbefa95b75608cf91e250be
braedonwatkins <watkins.braedon@gmail.com>
2023-02-08 15:39:49 -0500

    hotfix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
+  purge: ["*.{html,js,jsx,ts,tsx"],
   content: ["*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        serif: ['Ubuntu', 'Sans-serif'],
-      }
+        serif: ["Ubuntu", "Sans-serif"],
+      },
     },
   },
   plugins: [],
-}
+};
+
