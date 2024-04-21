3aa3d6339ecd2f78555f1cee0cba62352581d15a
braedonwatkins <watkins.braedon@gmail.com>
2024-04-21 12:44:00 -0400

    html & css spacing

--- resume/src/pages/Home.tsx
+++ resume/src/pages/Home.tsx
@@ -11,14 +11,22 @@ const Home = () => {
     }
   }, []); // Empty dependency array ensures this runs once on mount.
   return (
-    <div>
+    <div className="h-screen w-screen">
       <Navbar />
-      <body className="flex justify-between">
-        <div className="flex-col">
-          <div>SHOWCASE</div>
-          <div>PROJECT LIST</div>
-        </div>
-        <div className="flex-col">PROFILE</div>
+      <body className="flex flex-1 flex-col h-full">
+        <main className="flex flex-1 justify-between h-full">
+          <div className="left-main flex flex-col items-stretch justify-center flex-1">
+            <div className="flex flex-1 items-center justify-center h-full">
+              SHOWCASE
+            </div>
+            <div className="flex flex-1 items-center justify-center h-full">
+              PROJECT LIST
+            </div>
+          </div>
+          <div className="right-main flex flex-col items-center justify-center flex-1">
+            <div>PROFILE</div>
+          </div>
+        </main>
       </body>
     </div>
   );
