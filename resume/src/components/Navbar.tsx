1396b49056ba8ef24a679967dfe76c1953311136
braedonwatkins <watkins.braedon@gmail.com>
2024-04-21 10:57:19 -0400

    IDK style changes not feeling it

--- resume/src/components/Navbar.tsx
+++ resume/src/components/Navbar.tsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <header className="flex justify-between p-4">
-      <ul className="flex gap-8 pl-10">
+      <ul className="flex gap-10 pl-10">
         <Link to="/home">Home</Link>
         <Link to="/blog">Bloog</Link>
       </ul>
-      <ul className="flex gap-8 pl-10">
+      <ul className="flex gap-10 pr-10">
         <Link to="/about-me">About Me</Link>
       </ul>
     </header>
