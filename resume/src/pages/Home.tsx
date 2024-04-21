fd4efae107ff3a7f3ec6e11516ee6c8cb9354104
braedonwatkins <watkins.braedon@gmail.com>
2024-04-21 12:59:31 -0400

    asymmetric flex box spacing

--- resume/src/pages/Home.tsx
+++ resume/src/pages/Home.tsx
@@ -14,15 +14,15 @@ const Home = () => {
     <body className="flex flex-col h-screen w-screen">
       <Navbar />
       <main className="flex justify-between h-full">
-        <div className="left-main flex flex-col justify-center flex-1">
-          <div className="flex flex-1 items-center justify-center">
+        <div className="left-main flex flex-[3_1_0%] flex-col justify-center">
+          <div className="flex flex-[2_1_0%] items-center justify-center">
             SHOWCASE
           </div>
-          <div className="flex flex-1 items-center justify-center">
+          <div className="flex flex-[3_1_0%] items-center justify-center">
             PROJECT LIST
           </div>
         </div>
-        <div className="right-main flex flex-col items-center justify-center flex-1">
+        <div className="right-main flex flex-[2_1_0%] flex-col items-center justify-center">
           <div>PROFILE</div>
         </div>
       </main>
