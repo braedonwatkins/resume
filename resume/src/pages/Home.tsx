adbd1c62f243d4807b34e922511008518cc61908
braedonwatkins <watkins.braedon@gmail.com>
2024-04-21 11:27:47 -0400

    use debug outlines for spacing

--- resume/src/pages/Home.tsx
+++ resume/src/pages/Home.tsx
@@ -1,15 +1,24 @@
+import { useEffect } from "react";
 import { Navbar } from "../components/Navbar";
+import { debugOutline } from "../utils/debugOutline";
+
+const DEBUG_MODE = import.meta.env.VITE_DEBUG === "true";
 
 const Home = () => {
+  useEffect(() => {
+    if (DEBUG_MODE) {
+      debugOutline();
+    }
+  }, []); // Empty dependency array ensures this runs once on mount.
   return (
     <div>
       <Navbar />
-      <body>
-        <div>
+      <body className="flex justify-between">
+        <div className="flex-col">
           <div>SHOWCASE</div>
-          <div>PROJECT List</div>
+          <div>PROJECT LIST</div>
         </div>
-        <div>PROFILE</div>
+        <div className="flex-col">PROFILE</div>
       </body>
     </div>
   );
