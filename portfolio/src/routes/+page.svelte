bcca8e5b95c38f182c68a6f161b6a57a6d24275a
braedonwatkins <watkins.braedon@gmail.com>
2024-10-08 15:54:49 -0400

    commented conversion from react, rebuilding

--- portfolio/src/routes/+page.svelte
+++ portfolio/src/routes/+page.svelte
@@ -1,7 +1,29 @@
 <script>
-    let counter = 0;
+  import { onMount } from 'svelte';
+//  import { debugOutline } from '../utils/debugOutline';
+//  import Showcase from '../components/Showcase.svelte';
+//  import ProjectList from '../components/ProjectList.svelte';
+//  import Profile from '../components/Profile.svelte';
+
+  const DEBUG_MODE = import.meta.env.VITE_DEBUG === 'true';
+
+  onMount(() => {
+    if (DEBUG_MODE) {
+      // debugOutline();
+    }
+  });
 </script>
 
-<h1>Test Me</h1>
-<button on:click={() => counter++}> Increment Counter </button>
-<div>Counter: {counter}</div>
+<main class="flex justify-between h-full">
+  <div class="left-main flex flex-[3_1_0%] flex-col justify-center">
+    <!-- <Showcase /> -->
+    <!-- <ProjectList /> -->
+  </div>
+  <div class="right-main flex flex-[2_1_0%] flex-col items-center justify-center">
+    <!-- <Profile /> -->
+  </div>
+</main>
+
+<style>
+</style>
+
