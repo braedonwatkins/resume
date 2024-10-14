a35522317f917886516f42036d823fb711f40f7c
braedonwatkins <watkins.braedon@gmail.com>
2024-10-14 12:18:40 -0400

    Mock data, idk bout this rn

---
+++ portfolio/src/mocks/careers.ts
@@ -0,0 +1,32 @@
+import type { Career } from "../models/Career"
+
+const careers: Career[] = [
+    {
+        name: 'Full-Stack Software Enginer',
+        skills: []
+    },
+    {
+        name: 'Backend Software Engineer',
+        skills: []
+    },
+    {
+        name: 'Frontend Software Engineer',
+        skills: []
+    },
+    {
+        name: 'Embedded Software Engineer',
+        skills: []
+    },
+    {
+        name: 'Graphics Engineer',
+        skills: []
+    },
+    {
+        name: 'Web Developer',
+        skills: []
+    },
+]
+
+
+
+export default careers
